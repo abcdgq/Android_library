c29cc1038134c493dabb76aa2fb63f1402024cbb
Ding Guangqing <22371146@buaa.edu.cn>
2024-11-16 19:55:35 +0800

    把各标签借阅次数在借阅历史里加上了

--- app/src/main/java/com/xkx/book/activity/borrow/BorrowHistoryActivity.java
+++ app/src/main/java/com/xkx/book/activity/borrow/BorrowHistoryActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.xkx.book.R;
 import com.xkx.book.adapter.BorrowAdapter;
@@ -17,6 +18,7 @@ import com.xkx.book.enity.Book;
 import com.xkx.book.enity.Borrow;
 import com.xkx.book.util.ToastUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BorrowHistoryActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -26,10 +28,13 @@ public class BorrowHistoryActivity extends AppCompatActivity implements AdapterV
     private BookDBHelper nHelper;
     private BorrowAdapter borrowAdapter;
 
+    private TextView textView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrow_history);
+        textView1 = findViewById(R.id.tv_extra_info_1);
 
         lv_view_borrow = findViewById(R.id.lv_view_borrow);
         lv_view_borrow.setOnItemClickListener(this);
@@ -48,9 +53,42 @@ public class BorrowHistoryActivity extends AppCompatActivity implements AdapterV
         nHelper.openReadLink();
 
         List<Borrow> borrows = mHistory.queryAll();
+        List<Book> books = new ArrayList<>();
         for (Borrow borrow : borrows) {
             Log.e("ning", borrow.toString());
+            Book book = nHelper.queryById(borrow.getBorrowBookId()).get(0);
+            books.add(book);
         }
+
+        int[] numbers = {0, 0, 0, 0, 0,0}; // 创建一个包含这些值的数组，长度为 5即可，
+        for (Book book : books){
+            String tags = book.getBookTags();
+            // 使用 split 方法按照 & 分隔字符串
+            String[] tagArray = tags.split("&");
+
+            // 打印分隔后的标签
+            for (String tag : tagArray) {
+                if (tag.equals("文学与艺术")){
+                    numbers[0] = numbers[0] + 1;
+                }else if (tag.equals("科学与技术")){
+                    numbers[1] = numbers[1] + 1;
+                } else if (tag.equals("社会科学")){
+                    numbers[2] = numbers[2] + 1;
+                }else if (tag.equals("历史与人文")){
+                    numbers[3] = numbers[3] + 1;
+                }else if (tag.equals("生活与健康")){
+                    numbers[4] = numbers[4] + 1;
+                }
+            }
+        }
+        String s = "文学与艺术:" + numbers[0] + "次\n" +
+                "科学与技术:" + numbers[1] + "次\n" +
+                "社会科学:" + numbers[2] + "次\n" +
+                "历史与人文:" + numbers[3] + "次\n" +
+                "生活与健康:" + numbers[4] + "次";
+
+        textView1.setText(s);
+
         borrowAdapter = new BorrowAdapter(borrows, BorrowHistoryActivity.this);
         lv_view_borrow.setAdapter(borrowAdapter);
 
